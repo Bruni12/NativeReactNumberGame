1e784f3426ae76524d2b4a34e64e28a286c218fc
Bruni12 <brunnetteboy@gmail.com>
2020-02-26 18:13:45 -0700

    added fonts

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -19,47 +19,51 @@ const GameScreen = props => {
   const [currentGuess, setCurrentGuess] = useState(
     generateRandomBetween(1, 100, props.userChoice)
   );
-  const[rounds, setRounds] = useState (0);
+  const [rounds, setRounds] = useState(0);
   const currentLow = useRef(1);
   const currentHigh = useRef(100);
 
   const { userChoice, onGameOver } = props;
 
-  useEffect (() => {
-      if (currentGuess === userChoice) {
-          onGameOver(rounds);
+  useEffect(() => {
+    if (currentGuess === userChoice) {
+      onGameOver(rounds);
     }
   }, [currentGuess, userChoice, onGameOver]);
 
-    const nextGuessHandler = direction => {
-        if ((direction === 'lower' && currentGuess < props.userChoice)
-         || (direction === 'greater' && currentGuess > props.userChoice)
-         ) {
-          Alert.alert('Don\'t lie', 'You know this is wrong...',
-           [{text: 'Sorry', style: 'cancel'}
-        ]);
-        return;
-        }
-        if (direction === 'lower') {
-            currentHigh.current = currentGuess;
-        } else {
-            currentLow.current = currentGuess;
-        }
-        const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
-        setCurrentGuess(nextNumber);
-        setRounds(curRounds => curRounds + 1);
-     };
+  const nextGuessHandler = direction => {
+    if (
+      (direction === 'lower' && currentGuess < props.userChoice) ||
+      (direction === 'greater' && currentGuess > props.userChoice)
+    ) {
+      Alert.alert("Don't lie!", 'You know that this is wrong...', [
+        { text: 'Sorry!', style: 'cancel' }
+      ]);
+      return;
+    }
+    if (direction === 'lower') {
+      currentHigh.current = currentGuess;
+    } else {
+      currentLow.current = currentGuess;
+    }
+    const nextNumber = generateRandomBetween(
+      currentLow.current,
+      currentHigh.current,
+      currentGuess
+    );
+    setCurrentGuess(nextNumber);
+    setRounds(curRounds => curRounds + 1);
+  };
 
   return (
     <View style={styles.screen}>
       <Text>Opponent's Guess</Text>
       <NumberContainer>{currentGuess}</NumberContainer>
       <Card style={styles.buttonContainer}>
-        <Button title="LOWER" 
-        onPress={nextGuessHandler.bind(this, 'lower')}
-        />
-        <Button title="GREATER" 
-        onPress={nextGuessHandler.bind(this, 'greater')} 
+        <Button title="LOWER" onPress={nextGuessHandler.bind(this, 'lower')} />
+        <Button
+          title="GREATER"
+          onPress={nextGuessHandler.bind(this, 'greater')}
         />
       </Card>
     </View>
