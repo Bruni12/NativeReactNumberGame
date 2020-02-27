f708b45e59c20b45f0418a5f94fad97cb136a0c5
Bruni12 <brunnetteboy@gmail.com>
2020-02-26 20:11:20 -0700

    image end game screen, fonts

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -12,6 +12,8 @@ import {
 import Card from '../components/Card';
 import Input from '../components/Input';
 import NumberContainer from '../components/NumberContainer';
+import BodyText from '../components/BodyText';
+import TitleText from'../components/TitleText';
 import Colors from '../constants/colors';
 
 const StartGameScreen = props => {
@@ -49,7 +51,7 @@ const StartGameScreen = props => {
     if (confirmed) {
       confirmedOutput = (
         <Card style={styles.summaryContainer}>
-        <Text>You Selected</Text>
+        <BodyText>You Selected</BodyText>
         <NumberContainer>{selectedNumber}</NumberContainer>
         <Button title="START GAME" onPress={() =>props.onStartGame(selectedNumber)} />
       </Card>
@@ -62,10 +64,11 @@ const StartGameScreen = props => {
         Keyboard.dismiss();
      }}>   
       <View style={styles.screen}>
-      <Text style={styles.title}>The Game Screen!</Text>
+      <TitleText style={styles.title}>The Game Screen!</TitleText>
        <Card style={styles.inputContainer}>
-       <Text> Select a Number </Text>
-       <Input style={styles.input} 
+       <BodyText> Select a Number </BodyText>
+       <Input 
+          style={styles.input} 
           blurOnSubmit 
           autoCapitilize='none'
           autCorrect={false}
@@ -99,11 +102,13 @@ const styles = StyleSheet.create({
  screen: {
     flex: 1,
     padding: 10,
-    alignItems: 'center' 
+    alignItems: 'center'
+    
   },
   title: {
     fontSize: 20,
     marginVertical: 10,
+    fontFamily: 'open-sans-bold'
   },
   inputContainer: {
     width: 300,
@@ -126,6 +131,9 @@ const styles = StyleSheet.create({
   summaryContainer: {
     marginTop: 20,
     alignItems: 'center'
+  },
+  text: {
+    fontFamily: 'open-sans'
   }
 });
 
